d96cddddb58675b355f8b40bdfde74f72b254bbf
Gaetan <contact@gaetan.io>
2016-06-28 14:47:08 +0200

    Etape 2.

--- users.php
+++ users.php
@@ -1,39 +1,56 @@
 <?php
 
-  $my_users = array(
-    0 => array(
-      'id' => '1',
-      'name' => 'JP',
-      'mail' => 'jp@example.com',
-      'role' => array('admin'),
-    ),
-    1 => array(
-      'id' => '2',
-      'name' => 'DD',
-      'mail' => 'dd@example.com',
-      'role' => array('journalist'),
-    ),
-    2 => array(
-      'id' => '3',
-      'name' => 'MC',
-      'mail' => 'mc@example.com',
-      'role' => array('chief_editor'),
-    ),
-    3 => array(
-      'id' => '4',
-      'name' => 'JFX',
-      'mail' => 'jfx@example.com',
-      'role' => array('webmaster','chief_editor'),
-    ),
-  );
+// print '<pre>';
+// print_r($users);
+// print '</pre>';
 
-  // print '<pre>';
-  // print_r($my_users);
-  // print '</pre>';
+$users = array(
+	0 => array(
+		'id' => '1',
+		'name' => 'JP',
+		'mail' => 'jp@example.com',
+		'role' => array('admin'),
+	),
+	1 => array(
+		'id' => '2',
+		'name' => 'DD',
+		'mail' => 'dd@example.com',
+		'role' => array('journalist'),
+	),
+	2 => array(
+		'id' => '3',
+		'name' => 'MC',
+		'mail' => 'mc@example.com',
+		'role' => array('chief_editor'),
+	),
+	3 => array(
+		'id' => '4',
+		'name' => 'JFX',
+		'mail' => 'jfx@example.com',
+		'role' => array('webmaster','chief_editor'),
+	),
+);
 
-  foreach($my_users as $user) {
-    print 'Nom: ' . $user['name'] . '<br/>';
-    print 'Email:' . $user['mail'] . '<br/>';
-  }
+$markup = '<table class="table table-hover">';
+$markup .= '<thead> <tr> <th>#</th> <th>Name</th> <th>Email</th></tr> </thead>';
+$markup .= '<tbody>';
+
+foreach($users as $user) {
+	$markup .= '<tr>';
+	$markup .= generate_user_row($user);
+	$markup .= '</tr>';
+}
+
+$markup .= '</tbody>';
+$markup .= '</table>';
+print $markup;
+
+
+/*		FUNCTIONS 		*/
+
+function generate_user_row($user) {
+
+	return '<th scope="row">' . $user['id'] . '</th><td>' . $user['name'] . '</td><td>' . $user['mail'] . '</td>';
+}
 
 ?>
